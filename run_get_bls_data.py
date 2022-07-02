fb3b32fe349702401a5696d10ea5b1a794e48319
cpsingh1307 <107349064+cpsingh1307@users.noreply.github.com>
2022-07-02 19:15:32 +0530

    Add files via upload

---
+++ run_get_bls_data.py
@@ -0,0 +1,2 @@
+from c_bls_data import c_bls_data
+c_bls_data('595f423508a84a81a7053396ff67ccbc', r'C:\Users\Dell\Desktop\inflation.csv', ['CUSR0000SA0', 'CUSR0000SETB01', 'CUSR0000SAF1', 'CUSR0000SETA02'], 2003, 2022)
